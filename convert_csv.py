9f91184ff446719d5671071878d3f124a2e82132
Clifton Palmer <clifton.james.palmer@protonmail.com>
2020-05-03 22:16:46 -0500

    Updated with order number

--- convert_csv.py
+++ convert_csv.py
@@ -2,7 +2,8 @@ import sys
 import csv
 
 stamps_fieldnames_dynamic = {
-    'Order ID': 'Order Number',
+    'Order ID (required)': 'Order Number',
+    'Order Number': 'Order Number',
     'Order Date': 'Purchase Date',
     'Order Value': 'Net Total ($)',
     'Ship To - Name': 'Buyer Name',
@@ -15,10 +16,29 @@ stamps_fieldnames_dynamic = {
     'Notes - Internal': 'Variants',
     }
 stamps_fieldnames_static = {
+    'Order ID (required)': '',
+    'Order Date': '',
+    'Order Value': '',
+    'Requested Service': 'Standard Shipping',
+    'Ship To - Name': '',
+    'Ship To - Company': '',
+    'Ship To - Address 1': '',
+    'Ship To - Address 2': '',
+    'Ship To - Address 3': '',
+    'Ship To - State/Province': '',
+    'Ship To - City': '',
+    'Ship To - Postal Code': '',
+    'Ship To - Country': '',
+    'Ship To - Phone': '',
+    'Ship To - Email': '',
     'Total Weight in Oz': '4',
     'Dimensions - Length': '11',
     'Dimensions - Width': '8.5',
     'Dimensions - Height': '0.5',
+    'Notes - From Customer': '',
+    'Notes - Internal': '',
+    'Gift Wrap?': '',
+    'Gift Message': '',
     }
 
 stamps_fieldnames = stamps_fieldnames_dynamic.keys() + \
